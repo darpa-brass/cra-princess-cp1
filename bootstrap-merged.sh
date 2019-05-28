20c5f832d6517eabd3cd950e425fdd911055e84c
Mike Reposa <mreposa@cra.com>
2019-05-28 11:29:14 -0400

    More cleanup

--- bootstrap-merged.sh
+++ bootstrap-merged.sh
@@ -11,14 +11,14 @@ apt-get install -y build-essential tcl tk --no-install-recommends
 apt-get install -y libpq-dev vim --no-install-recommends
 apt-get install -y software-properties-common python-software-properties --no-install-recommends
 
-# Python 3.6 and packages
+# Python Packages
 apt-add-repository universe
 apt-get update
 # apt-get -y install python3.6 --no-install-recommends
-apt-get -y install python-dev --no-install-recommends
-# apt-get -y install python-pip --no-install-recommends
-apt-get -y install python-setuptools --no-install-recommends
-pip3 install --upgrade pip
+apt-get -y install python3-dev --no-install-recommends
+apt-get -y install python3-pip --no-install-recommends
+apt-get -y install python3-setuptools --no-install-recommends
+# pip3 install --upgrade pip
 pip3 install numpy
 pip3 install scipy
 pip3 install scikit-learn
