c6bcd252646543816000a8f318bbf8eaa22c8c29
Mike Reposa <mreposa@cra.com>
2019-05-28 11:10:35 -0400

    Remove Octave and Python

--- bootstrap-merged.sh
+++ bootstrap-merged.sh
@@ -16,23 +16,14 @@ apt-add-repository universe
 apt-get update
 # apt-get -y install python3.6 --no-install-recommends
 apt-get -y install python-dev --no-install-recommends
-apt-get -y install python-pip --no-install-recommends
+# apt-get -y install python-pip --no-install-recommends
 apt-get -y install python-setuptools --no-install-recommends
-pip install --upgrade pip
-
-pip install numpy
-pip install scipy
-pip install scikit-learn
-pip install pyorient
-pip install lxml
-
-# Brew and Octave
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-# test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-# test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-# test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-# echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-# brew install octave
+pip3 install --upgrade pip
+pip3 install numpy
+pip3 install scipy
+pip3 install scikit-learn
+pip3 install pyorient
+pip3 install lxml
 
 # Access Control Settings
 chmod 777 ./start
