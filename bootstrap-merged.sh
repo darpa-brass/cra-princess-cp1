a0f6ff07198c5d3270ee6c6021b21a451a74e138
Mike Reposa <mreposa@cra.com>
2019-06-03 15:24:33 -0400

    Adding ortools package

--- bootstrap-merged.sh
+++ bootstrap-merged.sh
@@ -11,19 +11,29 @@ apt-get install -y build-essential tcl tk --no-install-recommends
 apt-get install -y libpq-dev vim --no-install-recommends
 apt-get install -y software-properties-common python-software-properties --no-install-recommends
 
-# Python Packages
+# Python 3.6 and packages
 apt-add-repository universe
 apt-get update
 # apt-get -y install python3.6 --no-install-recommends
-apt-get -y install python3-dev --no-install-recommends
-apt-get -y install python3-pip --no-install-recommends
-apt-get -y install python3-setuptools --no-install-recommends
-# pip3 install --upgrade pip
-pip3 install numpy
-pip3 install scipy
-pip3 install scikit-learn
-pip3 install pyorient
-pip3 install lxml
+apt-get -y install python-dev --no-install-recommends
+apt-get -y install python-pip --no-install-recommends
+apt-get -y install python-setuptools --no-install-recommends
+pip install --upgrade pip
+
+pip install numpy
+pip install scipy
+pip install scikit-learn
+pip install pyorient
+pip install lxml
+pip install ortools
+
+# Brew and Octave
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+# test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+# test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+# echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+# brew install octave
 
 # Access Control Settings
-# chmod 777 ./start
+chmod 777 ./start
