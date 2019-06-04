b672ca1bc7b1d85ff893ba3080c173c8076b5fcf
Mike Reposa <mreposa@cra.com>
2019-06-04 11:20:14 -0400

    Some cleanup

--- bootstrap-merged.sh
+++ bootstrap-merged.sh
@@ -9,16 +9,15 @@ apt-get update
 # Common Packages
 apt-get install -y build-essential tcl tk --no-install-recommends
 apt-get install -y libpq-dev vim --no-install-recommends
-apt-get install -y software-properties-common python3-software-properties --no-install-recommends
+apt-get install -y software-properties-common --no-install-recommends
 
-# Python 3.6 and packages
+# Python Packages
 apt-add-repository universe
 apt-get update
-# apt-get -y install python3.6 --no-install-recommends
-apt-get -y install python3-dev --no-install-recommends
-apt-get -y install python3-pip --no-install-recommends
-apt-get -y install python3-setuptools --no-install-recommends
-# pip3 install --upgrade pip
+apt-get install -y python3-software-properties --no-install-recommends
+apt-get install -y python3-dev --no-install-recommends
+apt-get install -y python3-pip --no-install-recommends
+apt-get install -y python3-setuptools --no-install-recommends
 
 pip3 install numpy
 pip3 install scipy
